84036d0d299e9b80ebfe843f0afbe8abf993018f
Rohan Yadav <rajrohanyadav@gmail.com>
2025-01-10 10:21:05 +0530

    add rust plugins

--- .config/nvim/lua/core/mappings.lua
+++ .config/nvim/lua/core/mappings.lua
@@ -74,6 +74,28 @@ M.general = {
   },
 }
 
+-- Nvim DAP
+M.dap = {
+  n = {
+    ["<Leader>dl"] = { "<cmd>lua require'dap'.step_into()<CR>", "Debugger step into" },
+    ["<Leader>dj"] = { "<cmd>lua require'dap'.step_over()<CR>", "Debugger step over" },
+    ["<Leader>dk"] = { "<cmd>lua require'dap'.step_out()<CR>", "Debugger step out" },
+    ["<Leader>dc"] = { "<cmd>lua require'dap'.continue()<CR>", "Debugger continue" },
+    ["<Leader>db"] = { "<cmd>lua require'dap'.toggle_breakpoint()<CR>", "Debugger toggle breakpoint" },
+    ["<Leader>dd"] = { "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>", "Debugger set conditional breakpoint" },
+    ["<Leader>de"] = { "<cmd>lua require'dap'.terminate()<CR>", "Debugger reset" },
+    ["<Leader>dr"] = { "<cmd>lua require'dap'.run_last()<CR>", "Debugger run last" },
+  },
+}
+
+-- rustaceanvim
+M.rustaceanvim = {
+  plugin = true,
+  n = {
+    ["<Leader>dt"] = { "<cmd>lua vim.cmd('RustLsp testables')<CR>", "Debugger testables" },
+  },
+}
+
 M.tabufline = {
   plugin = true,
 
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -176,12 +176,12 @@ local default_plugins = {
   {
     "numToStr/Comment.nvim",
     keys = {
-      { "gcc", mode = "n", desc = "Comment toggle current line" },
-      { "gc", mode = { "n", "o" }, desc = "Comment toggle linewise" },
-      { "gc", mode = "x", desc = "Comment toggle linewise (visual)" },
-      { "gbc", mode = "n", desc = "Comment toggle current block" },
-      { "gb", mode = { "n", "o" }, desc = "Comment toggle blockwise" },
-      { "gb", mode = "x", desc = "Comment toggle blockwise (visual)" },
+      { "gcc", mode = "n",          desc = "Comment toggle current line" },
+      { "gc",  mode = { "n", "o" }, desc = "Comment toggle linewise" },
+      { "gc",  mode = "x",          desc = "Comment toggle linewise (visual)" },
+      { "gbc", mode = "n",          desc = "Comment toggle current block" },
+      { "gb",  mode = { "n", "o" }, desc = "Comment toggle blockwise" },
+      { "gb",  mode = "x",          desc = "Comment toggle blockwise (visual)" },
     },
     init = function()
       require("core.utils").load_mappings "comment"
@@ -242,6 +242,78 @@ local default_plugins = {
       require("which-key").setup(opts)
     end,
   },
+
+  {
+    "mrcjkb/rustaceanvim",
+    version = '^5', -- Recommended
+    lazy = false,   -- This plugin is already lazy
+    config = function()
+      local mason_registry = require("mason-registry")
+      local codelldb = mason_registry.get_package("codelldb")
+      local extension_path = codelldb:get_install_path() .. "/extension/"
+      local codelldb_path = extension_path .. "adapter/codelldb"
+      local cfg = require("rustaceanvim.config")
+
+      vim.g.rustaceanvim = {
+        dap = {
+          adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
+        },
+      }
+    end
+  },
+
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+    local dap, dapui = require("dap"), require("dapui")
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+    end
+  },
+
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+    config = function()
+      require("dapui").setup()
+    end
+  },
+
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+
+  {
+    "saecki/crates.nvim",
+    ft = {"toml"},
+    config = function()
+      require("crates").setup {
+        completion = {
+          cmp = {
+            enabled = true
+          },
+        },
+      }
+      require("cmp").setup.buffer({
+        sources = { { name = "crates" } }
+      })
+    end
+  },
+
 }
 
 local config = require("core.utils").load_config()
