7230b1f23d80d4b08b51b54e021471866390d41f
Rohan Yadav <rajrohanyadav@gmail.com>
2025-07-27 12:18:26 +0530

    update lsp configs

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -15,13 +15,52 @@ M.base46 = {
             italic = true
         }
     },
-    theme_toggle = { "onedark", "one_light" },
+    theme_toggle = {"onedark", "one_light"}
 }
 
 -- M.nvdash = { load_on_startup = true }
 M.ui = {
     tabufline = {
         lazyload = false
+    },
+    telescope = {
+        style = "borderless"
+    } -- borderless / bordered
+}
+
+M.lsp = {
+    signature = true
+}
+
+M.term = {
+    base46_colors = true,
+    winopts = {
+        number = false,
+        relativenumber = false
+    },
+    sizes = {
+        sp = 0.3,
+        vsp = 0.2,
+        ["bo sp"] = 0.3,
+        ["bo vsp"] = 0.2
+    },
+    float = {
+        relative = "editor",
+        row = 0.3,
+        col = 0.25,
+        width = 0.5,
+        height = 0.4,
+        border = "single"
+    }
+}
+
+M.colorify = {
+    enabled = true,
+    mode = "virtual", -- fg, bg, virtual
+    virt_text = "󱓻 ",
+    highlight = {
+        hex = true,
+        lspvars = true
     }
 }
 
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,18 +1,24 @@
-return {{
+return {
+  {
     "stevearc/conform.nvim",
     event = 'BufWritePre',
     opts = require "configs.conform"
-},
-{
+  },
+  {
     "neovim/nvim-lspconfig",
     config = function()
-        require "configs.lspconfig"
+      require "configs.lspconfig"
     end
-}, 
-{ import = "nvchad.blink.lazyspec" },
-{
+  },
+  {
+    "mfussenegger/nvim-lint",
+    opts = require "configs.lint"
+  },
+  { import = "nvchad.blink.lazyspec" },
+  {
     "nvim-treesitter/nvim-treesitter",
     opts = {
-        ensure_installed = {"vim", "lua", "vimdoc", "html", "css"}
+      ensure_installed = { "vim", "lua", "vimdoc", "html", "css", "go", "markdown", "markdown_inline", "python", "rust" }
     }
-}}
+  }
+}
